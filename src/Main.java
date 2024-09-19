5e696768d85132b56a87f5dd0a541801222d0a26
Dominic <Domdombossy@gmail.com>
2024-09-19 13:53:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double summerCost = 148.95;
+        double springCost = 273.49;
+        double autumnCost = 172.00;
+        double winterCost = 114.00;
+        double yearlyCost = 0.00;
+
+
+        yearlyCost = summerCost + springCost + autumnCost + winterCost;
+        System.out.println("Your summer costs are: " + summerCost);
+        System.out.println("Your spring costs are: " + springCost);
+        System.out.println("Your fall costs are: " + autumnCost);
+        System.out.println("Your winter costs are: " + winterCost);
+        System.out.println("Your yearly costs are: " + yearlyCost);
+
+
+    }
+
+
+}
